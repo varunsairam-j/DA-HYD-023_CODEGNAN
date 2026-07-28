72d4c7ca2a784ab328d92ebf703fdaf40ca39a46
varunsairam-j <varunsairamjoolapalli2005@gmail.com>
2026-07-28 13:45:05 +0530

    Tokens

---
+++ Day-1.py
@@ -0,0 +1,22 @@
+print("Hello Varun") ; '''This is my Name'''
+'''
+Tokens are the smallest units in a program
+'''
+#Identifiers - These are the names being allotted to variables, functions, classes...
+#We Cannot start with a number.
+#We can start with uppercase / lowercase letter (A-Z, a-z) but not special characters (@,$,#,....)
+#No Space between letters, where as we can also use underscore as an identifier (_)
+#Python is a Case-Sensitive language.
+name = 'Codegnan'
+print(name)
+email_id = 'varunsairamjoolapalli2005@gmail.com'
+print(email_id)
+hi = 'hello'
+print (hi)
+
+#Multi assignment of variables
+
+name,location,age = 'Varun','Hyderabad',20
+print(name)
+print(location)
+print(age)
